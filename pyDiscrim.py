daff1e518f7a61eacd66b16de092a54c8e7b968a
Chris Deister <cdeister@brown.edu>
2015-05-17 11:14:46 -0400

    0.99 -- Python with terminal feedback
    So the python script is ready for production use. However, there are
    some things to be aware of, and some BIG gotchas.
    
    1) I took out a timer for controlling the runs and instead loop until a
    certain amount of trials have been run. The variable that controls this
    is “trialsToRun.”
    
    2) The data gets saved out as a CSV the end of the run! This means if
    you are running the script from a bash shell (or equivalent) and then
    interrupt the run, you will be hosed and have no record of anything
    (!!!!). So, you need to let the whole thing play out. Alternatively,
    you can run the script in an interactive python session (iPython) and
    then if you abort the script, the lists that contain all the data will
    still be in memory and you can salvage the run.
    
    3) You can get text feedback of the trial and outcome (hit/miss) by
    turning on the “giveTerminalFeedback” flag.
    
    **** To Do, Etc. ****
    I am working on implementing a pickle stream, which should fix 2.
    I am working on implementing some form of real time(ish) plotting.

--- pyDiscrim.py
+++ pyDiscrim.py
@@ -24,17 +24,20 @@ tOffset=2.2
 latch=0
 n=1
 currentState=1
+currentTrial=1
 trialGrace=3000
 bufferSize=499 # The crapier the mouse the higher this needs to be
 stopThreshold=1
+giveTerminalFeedback=1
+trialsToRun=100
 
 arduino = serial.Serial('/dev/cu.usbmodem1421', 115200) #Creating our serial object named arduinoData
-#arduino.write('1')
+arduino.write('1')
 
-
+displayLatch=0
 startTime = time.time()
 currentTime=0    
-while currentTime<=60+tOffset:
+while currentTrial<=trialsToRun: #currentTime<=60+tOffset:
     while (arduino.inWaiting()==0): #Wait here until there is data
         pass #do nothing
     if currentState==1:    
@@ -69,8 +72,18 @@ while currentTime<=60+tOffset:
         currentState=states[-1]
         stimDif=leftExpectedVal[-1]-rightExpectedVal[-1]
         if timeInStates[-1]>trialGrace and numpy.mean(numpy.abs(deltas[-199:-1]))<stopThreshold and positions[-1]>stimChangePositions[-1] and positions[-1]<=stimChangePositions[-1]+stimChangeRanges[-1]:
+            if displayLatch==0:
+                displayLatch=1
+                print("trial #"),
+                print(trialCount[-1]),
+                print(" = hit")
             arduino.write('4')
         elif timeInStates[-1]>trialGrace and numpy.mean(numpy.abs(deltas[-199:-1]))<stopThreshold and positions[-1]<stimChangePositions[-1] or positions[-1]>stimChangePositions[-1]+stimChangeRanges[-1]:  
+            if displayLatch==0:
+                displayLatch=1
+                print("trial #"),
+                print(trialCount[-1]),
+                print(" = miss")
             arduino.write('5')
             
     elif currentState==3:
@@ -122,6 +135,11 @@ while currentTime<=60+tOffset:
         currentState=states[-1]            
     n=n+1
     currentTime=time.time()-startTime
+    currentTrial=trialCount[-1]
+    if n>3 and giveTerminalFeedback==1:
+        trialDif=trialCount[-1]-trialCount[-2]
+        if trialDif==1:
+            displayLatch=0     #This is for terminal feedback of trial flow
 
 exportArray=numpy.array([states,positions,deltas,timeInStates,totalTime,trialCount,stimChangePositions,stimChangeRanges,clickTrainLeft,clickTrainRight,pythonTime])
 exportArray = exportArray[numpy.newaxis].T
